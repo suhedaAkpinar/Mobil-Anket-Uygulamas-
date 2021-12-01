e64a9e17ad5d21bdff4703bba294cc673bb1f1c7
Suheda <75789656+suhedaAkpinar@users.noreply.github.com>
2021-12-01 16:16:06 +0300

    Add files via upload

---
+++ java/net/simplifiedcoding/androidphpmysql/Constants.java
@@ -0,0 +1,15 @@
+package net.simplifiedcoding.androidphpmysql;
+
+/**
+ * Created by Belal on 25/11/16.
+ */
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.1.101/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
